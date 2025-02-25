764f2b2ff8982117a2aff7ecd8e1e72655e84097
Felipe <articmario101@gmail.com>
2025-02-25 12:17:26 -0500

    Added HelloWorld funct to index.js, installed react-native-calendars to the app

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Calendar } from 'react-native-calendars';
+
+function HelloWorld() {
+  return <h1 className="greeting">Hello World!</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>
 );
 
